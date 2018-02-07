93bfbdeb65e8481f9f04ffee9ed312b1b5b9b645
Singer  Daniel <dsinger@ethz.ch>
2018-02-07 14:37:09 +0100

    .Added image switch to HeaderToImageConverter .Fixed Typos in MainWindow.xaml

--- HeaderToImageConverter.cs
+++ HeaderToImageConverter.cs
@@ -16,7 +16,18 @@ namespace WpfTreeView
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return new BitmapImage(new Uri($"pack://application:,,,/Images/{value}.png"));
+            var image = "Images/file.png";
+
+            switch ((DirectoryItemType)value)
+            {
+                case DirectoryItemType.Drive:
+                    image = "Images/drive.png";
+                    break;
+                case DirectoryItemType.Folder:
+                    image = "Images/folder-closed.png";
+                    break;
+            }
+            return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
